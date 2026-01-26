e23f4f3c8caff80aa05936eb97c1aec435ae5f88
Serhii Musienko <serhii.musienko@pitchbook.com>
2026-01-26 18:08:10 +0200

    Docs: add screenshots

--- homework-1/src/main/java/com/example/banking/web/AccountController.java
+++ homework-1/src/main/java/com/example/banking/web/AccountController.java
@@ -6,7 +6,6 @@ import com.example.banking.service.TransactionService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.math.BigDecimal;
